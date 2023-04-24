63d95c0825a3715e143caaa2a86898f95493ef6e
Max Goodhart <c@chromakode.com>
2023-04-23 23:00:58 -0700

    Generate URLs with /shape prefix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://chromakode.com/shape/',
+  base: '/shape',
   experimental: { assets: true },
   image: {
     service: 'astro/assets/services/sharp',
