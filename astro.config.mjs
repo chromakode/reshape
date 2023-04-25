b53333523eeb0723584854820603ab85e25767f7
Max Goodhart <c@chromakode.com>
2023-04-24 22:44:03 -0700

    Compress HTML

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config'
 
+import compress from 'astro-compress'
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://chromakode.com/shape/',
   base: '/shape',
-  experimental: { assets: true },
+  experimental: {
+    assets: true,
+  },
   image: {
     service: 'astro/assets/services/sharp',
   },
+  integrations: [
+    compress({ html: { ignoreCustomComments: [/[-\. ]+/] }, img: false }),
+  ],
 })
